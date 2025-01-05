3d60bbb7333aa8f795987aa928ad80b6eac1ea3e
HemantDevgan <hemantdevgan.0099@gmail.com>
2025-01-05 13:34:34 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const Mongo_url = "mongodb://127.0.0.1:27017/new_CRM"
-const mongo_url = "mongodb+srv://hemantdevgan0098:<db_password>@cluster0.tkyyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const Mongo_url = "mongodb+srv://hemantdevgan0098:Hem0098@cluster0.tkyyr.mongodb.net/"
 
 const db = ()=>{
     try{
